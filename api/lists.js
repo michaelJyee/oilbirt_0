ded1461545d563d7b4f8411e411e44754ec012ff
michael yee <michael.yee@triblio.com>
2018-11-19 00:46:47 -0500

    execute works.....

--- api/lists.js
+++ api/lists.js
@@ -1,5 +1,15 @@
 /*jshint esversion: 6 */ 
+var Sequelize = require('sequelize');
 var Lists = require('../models/sql/lists.js');
+var Contacts = require('../models/sql/contacts.js');
+var _ = require('lodash');
+const Op = Sequelize.Op;
+
+const operators = {
+  CONTAINS: Op.iLike,
+  NOTCONTAINS: Op.notILike,
+  NOTEQ: Op.not
+};
 
 exports.newLists = function(req, res){
   Lists.create({name:req.body.name, querymodel: "[]"})
@@ -55,9 +65,7 @@ exports.execute = function(req, res){
   Lists.findOne({where:{id:req.params.id}})
     .then(function(data){
       var q = JSON.parse(data.querymodel);
-      q.forEach(function(param){
-        console.log(param);
-      });
+      _queryModelToObject(q);
 
       res.send(data);
     })
@@ -66,3 +74,31 @@ exports.execute = function(req, res){
       res.send(400);
   });
 };
+
+function _queryModelToObject(model){
+  var where = {};
+
+  model.forEach(function(param){
+    if(!where[param.field]) where[param.field] = {};
+
+    if(param.opp === 'NOTCONTAINS' || param.opp === 'NOTCONTAINS'){
+      where[param.field][operators[param.opp]] = `%${param.value}%`;
+    }
+    else if(param.opp === 'EQ'){
+      where[param.field] = param.value;
+    }
+    else if(param.opp === 'NOTEQ'){
+       where[param.field][operators[param.opp]] = `${param.value}`;
+    }
+
+    console.log(param);
+  });
+
+  console.log("where=>",where);
+  Contacts.findAll({where:where})
+  .then(data => {
+    console.log(data);
+  });
+
+  return where;
+}
